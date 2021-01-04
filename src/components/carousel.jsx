bf8e3b1731d12c0d890e2602e47f3ca2b621e69d
xren935 <59517115+xren935@users.noreply.github.com>
2021-01-03 23:48:48 -0800

    finished header and home page 😋

---
+++ src/components/carousel.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../static/carousel.css";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import Carousel from 'react-bootstrap/Carousel'
+import bento0 from '../static/img/bento0.png'; 
+import bento1 from '../static/img/bento1.png'; 
+import bento2 from '../static/img/bento2.png'; 
+
+class CarouselGallery extends React.Component {
+  constructor(props){
+    super(props);
+  
+    this.state = {
+     
+    };
+  }
+
+  render() {
+    return (
+        <div className="customized-carousel-gallery" style={{height: "100vh"}}>
+            <Carousel style={{height: "100%"}}>
+                <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={bento0}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>Welcome To</h3>
+                    <br></br>
+                    <h1>Wong's Chinese Restaurant</h1>
+                    <br></br>
+                    <h4><i>a Chinese food restaurant located in Langley, BC</i></h4>
+                </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={bento1}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>Welcome To</h3>
+                    <br></br>
+                    <h1>Wong's Chinese Restaurant</h1>
+                    <br></br>
+                    <h4><i>a Chinese food restaurant located in Langley, BC</i></h4>
+                </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={bento2}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>Welcome To</h3>
+                    <br></br>
+                    <h1>Wong's Chinese Restaurant</h1>
+                    <br></br>
+                    <h4><i>a Chinese food restaurant located in Langley, BC</i></h4>
+                </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </div>
+    );
+  }
+}
+
+export default CarouselGallery;
---
+++ src/components/home.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'font-awesome/css/font-awesome.min.css';
+import '../static/home.css';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from "react-bootstrap";
+import { Spring } from "react-spring/renderprops";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import Logo from '../static/img/logo.png';
+import { FaConciergeBell, FaMapMarkerAlt, FaPhoneAlt} from "react-icons/fa";
+
+class Home extends React.Component {
+  constructor(props){
+    super(props);
+  
+    this.state = {
+     
+    };
+  }
+
+
+  render() {
+    Aos.init({interval: 1000});
+    var FontAwesome = require('react-fontawesome');
+    return (
+      <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
+      {(props) => (
+        <div style={props}>
+          <Navbar className="customized-nav" expand="lg">
+          {/* <Navbar.Logo></Navbar.Logo> */}
+          <Navbar.Brand href="#home">
+              <img src={Logo} style={{ width: 200, marginTop: -7 }}></img>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">HOME</Nav.Link>
+              <Nav.Link href="#about">ABOUT</Nav.Link>
+              <Nav.Link href="#link">MENU</Nav.Link>
+              <Nav.Link href="#link" style={{ marginRight: "100px"}}>CONTACT</Nav.Link>
+              <Nav.Link><FaConciergeBell/></Nav.Link>
+              <Nav.Link><FaMapMarkerAlt/></Nav.Link>
+              <Nav.Link><FaPhoneAlt/></Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+       
+    </div>
+      )}
+    </Spring>
+    );
+  }
+}
+
+export default Home;
