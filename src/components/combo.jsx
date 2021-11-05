f567243f740a99e3958eab6cb8a5f706e751c329
xren935 <59517115+xren935@users.noreply.github.com>
2021-11-05 12:11:06 -0700

    Update prices 📈

--- src/components/combo.jsx
+++ src/components/combo.jsx
@@ -1,90 +1,126 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import 'font-awesome/css/font-awesome.min.css';
-import '../static/combo.css';
+import "font-awesome/css/font-awesome.min.css";
+import "../static/combo.css";
 import { Spring } from "react-spring/renderprops";
 import Aos from "aos";
 import "aos/dist/aos.css";
 
-var familydinner = ['Dinner For Two', 'Dinner For Three', 'Dinner For Four', 'Dinner For Six (Large Order)', 'Dinner For Eight (Large Order)']
-var familyprices = [30, 45, 60, 90, 115];
-var familyitems = ['2 Egg Rolls, Beef Chop Suey, Sweet & Sour Pork, Chicken Chow Mein', '3 Egg Rolls, Beef Chop Suey, Sweet & Sour Pork, Chicken Chow Mein, Pork Fried Rice', '4 Egg Rolls, Special Chop Suey, Sweet & Sour Pork, Chicken Chow Mein, Pork Fried Rice, Almond Chicken', '6 Egg Rolls, Deep Fried Prawn, Chicken Chow Mein, Special Chop Suey, Almond Guy Ding, Pork Fried Rice, Almond Chicken, Sweet & Sour Pork', '8 Egg Rolls, Deep Fried Prawn, Chicken Chow Mein, Special Chop Suey, Almond Guy Ding, Pork Fried Rice, Almond Chicken, Sweet & Sour Pork, Chicken Egg Foo Young'];
+var familydinner = [
+  "Dinner For Two",
+  "Dinner For Three",
+  "Dinner For Four",
+  "Dinner For Six (Large Order)",
+  "Dinner For Eight (Large Order)",
+];
+var familyprices = [30, 45, 60, 90, 110];
+var familyitems = [
+  "2 Egg Rolls, Beef Chop Suey, Sweet & Sour Pork, Chicken Chow Mein",
+  "3 Egg Rolls, Beef Chop Suey, Sweet & Sour Pork, Chicken Chow Mein, Pork Fried Rice",
+  "4 Egg Rolls, Special Chop Suey, Sweet & Sour Pork, Chicken Chow Mein, Pork Fried Rice, Almond Chicken",
+  "6 Egg Rolls, Deep Fried Prawn, Chicken Chow Mein, Special Chop Suey, Almond Guy Ding, Pork Fried Rice, Almond Chicken, Sweet & Sour Pork",
+  "8 Egg Rolls, Deep Fried Prawn, Chicken Chow Mein, Special Chop Suey, Almond Guy Ding, Pork Fried Rice, Almond Chicken, Sweet & Sour Pork, Chicken Egg Foo Young",
+];
 
-var forone = ['Combination A', 'Combination B', 'Combination C', 'Combination D'];
-var foroneprices = [13, 13, 16, 13]; 
-var foroneitems = ['Pork Fried Rice, Chicken Chow Mein, Sweet & Sour Pork', 'Chicken Chow Mein, Chicken Chop Suey, Sweet & Sour Chicken', 'Chicken Chow Mein, Chicken Chop Suey, Sweet & Sour Pork, Deep Fried Prawns', 'Pork Fried Rice, Sweet & Sour Pork, Almond Chicken'];
+var forone = [
+  "Combination A",
+  "Combination B",
+  "Combination C",
+  "Combination D",
+];
+var foroneprices = [13, 13, 16, 13];
+var foroneitems = [
+  "Pork Fried Rice, Chicken Chow Mein, Sweet & Sour Pork",
+  "Chicken Chow Mein, Chicken Chop Suey, Sweet & Sour Chicken",
+  "Chicken Chow Mein, Chicken Chop Suey, Sweet & Sour Pork, Deep Fried Prawns",
+  "Pork Fried Rice, Sweet & Sour Pork, Almond Chicken",
+];
 
 class Combo extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-  
-    this.state = {
-     
-    };
+
+    this.state = {};
   }
 
-
   render() {
-    Aos.init({interval: 1000});
-    
-    function getItemList(items, prices, details){
-        var itemList = items.map(function(item, index){
-          const price = prices[index]; 
-          const detail = details[index];
-          return <div>
-                  <section>
-                    <h5 key={index}>{item}</h5>
-                    <span>......................................................................................................................................................................................................................................................................................</span>
-                    <h5 key={index}>${price}</h5>
-                  </section>
-                  <p key={index}>{detail}</p>
-                </div>
-        });
-        return itemList;
-      }
+    Aos.init({ interval: 1000 });
 
-    var familyList = getItemList(familydinner, familyprices, familyitems); 
-    var foroneList = getItemList(forone, foroneprices, foroneitems); 
+    function getItemList(items, prices, details) {
+      var itemList = items.map(function (item, index) {
+        const price = prices[index];
+        const detail = details[index];
+        return (
+          <div>
+            <section>
+              <h5 key={index}>{item}</h5>
+              <span>
+                ......................................................................................................................................................................................................................................................................................
+              </span>
+              <h5 key={index}>${price}</h5>
+            </section>
+            <p key={index}>{detail}</p>
+          </div>
+        );
+      });
+      return itemList;
+    }
+
+    var familyList = getItemList(familydinner, familyprices, familyitems);
+    var foroneList = getItemList(forone, foroneprices, foroneitems);
 
     return (
       <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
-      {(props) => (
-        <div style={props}>
+        {(props) => (
+          <div style={props}>
             <div className="combo" id="combo">
-                <div className="content">
-                    <h3>Combo</h3>
-                    <h3 className="subtitle">Lunch Special</h3>
-                    <br></br>
-                    <h4>Available 11:30 AM - 3:00 PM</h4>
-                    <h4 style={{color: "#e68a00"}}>Create your own combo!</h4>
-                    <h4 style={{color: "#e68a00"}}><b>3 items - $9.95</b></h4>
-                    <h4 style={{color: "#e68a00"}}><b>4 items - $11.95</b></h4>
-                    <br></br>
-                    <h4>Choose from:</h4>
-                    <ul style={{fontSize: "20px", listStyle: "none"}}>
-                      <li>Pork Fried Rice</li>
-                      <li>Mushroom Chow Mein</li>
-                      <li>Vegetable Chop Suey</li>
-                      <li>Sweet & Sour Pork</li>
-                      <li>Sweet & Sour Chicken Balls</li>
-                      <li>Almond Chicken</li>
-                      <li>Deep Fried Prawns</li>
-                      <li>Chicken with Black Bean Sauce</li>
-                      <li>Egg Roll</li>
-                      <li>Beef with Brocooli</li>
-                      <li>Almond Guy Ding</li>
-                      <li>Dry Ribs</li>
-                    </ul> 
-                    <h3 className="subtitle">Special Family Dinner</h3>
-                    <br></br>
-                    {familyList}
-                    <h3 className="subtitle">Special Family Dinner</h3>
-                    {foroneList}
-                </div>
+              <div className="content">
+                <br></br>
+                <br></br>
+                FREE HOME DELIVERY (within 4km and a minimum order of $40)
+                <br></br>
+                PICK-UP ORDER: 10% OFF (pay with cash and a minimum order of
+                $40)
+                <br></br>
+                5% OFF (pay with a credit or debit card and a minimum order of
+                $40)
+                <h3>Combo</h3>
+                <h3 className="subtitle">Lunch Special</h3>
+                <br></br>
+                <h4>Available 11:30 AM - 3:00 PM</h4>
+                <h4 style={{ color: "#e68a00" }}>Create your own combo!</h4>
+                <h4 style={{ color: "#e68a00" }}>
+                  <b>3 items - $10.50</b>
+                </h4>
+                <h4 style={{ color: "#e68a00" }}>
+                  <b>4 items - $13.00</b>
+                </h4>
+                <br></br>
+                <h4>Choose from:</h4>
+                <ul style={{ fontSize: "20px", listStyle: "none" }}>
+                  <li>Pork Fried Rice</li>
+                  <li>Mushroom Chow Mein</li>
+                  <li>Vegetable Chop Suey</li>
+                  <li>Sweet & Sour Pork</li>
+                  <li>Sweet & Sour Chicken</li>
+                  <li>Almond Chicken</li>
+                  <li>Deep Fried Prawns</li>
+                  <li>Chicken with Black Bean Sauce</li>
+                  <li>Egg Roll</li>
+                  <li>Beef with Brocooli</li>
+                  <li>Almond Guy Ding</li>
+                  <li>Dry Ribs</li>
+                </ul>
+                <h3 className="subtitle">Special Family Dinner</h3>
+                <br></br>
+                {familyList}
+                <h3 className="subtitle">Special Family Dinner</h3>
+                {foroneList}
+              </div>
             </div>
-        </div>
-      )}
-    </Spring>
+          </div>
+        )}
+      </Spring>
     );
   }
 }
--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -16,31 +16,31 @@ class Menu extends React.Component {
 
   render() {
     var appetizers = ['Crisp Egg Roll', 'Honey B.B.Q. Pork', 'Chinese Sausages', 'Deep Fried Wontons', 'Tofu with Pepper Salt', 'Deep Fried Chicken Wings', 'Honey Garlic Chicken Wings', 'Golden Fried Dumplings'];
-    var appetizersprices = [2.50, 10.00, 10.00, 10.00, 10.00, 12.00, 13.00, 11.00];
+    var appetizersprices = [2.50, 10.00, 10.00, 11.00, 11.00, 12.00, 13.00, 11.00];
     var soups = ['Wonton Soup', 'Wor Wonton Soup', 'Hot and Sour Soup', 'Chicken Crean Corn Soup', 'Egg Swirl Soup'];
-    var soupsprices = ['11 (Large) / $6 (Small)', '16 (Large) / $9 (Small)', '16 (Large) / $9 (Small)', '11 (Large) / $6 (Small)', '11 (Large) / $6 (Small)'];
+    var soupsprices = ['11 (Large) / $6 (Small)', '16 (Large) / $9 (Small)', '16 (Large) / $9 (Small)', '11 (Large) / $8 (Small)', '11 (Large) / $6 (Small)'];
     var wraps = ['Prok Lettuce Wrap', 'Beef Lettuce Wrap', 'Chicken Lettuce Wrap', 'Special Lettuce Wrap'];
     var wrapsprices = [16, 16, 16, 16];
     var hotpots = ['Beef Ginger, Green Onion Hot Pot', 'Beef Satay Hot Pot', 'Chicken Hot Pot', 'Vegetable & Tofu Hot Pot', 'Prawn Hot Pot'];
     var hotpotsprices = [16, 16, 16, 14, 16];
     var ribs = ['Sweet & Sour Pork', 'Dry Garlic Pork', 'Honey Garlic Pork', 'Honey Garlic Ribs', 'Dry Garlic Ribs'];
-    var ribsprices = [14, 14, 15, 15, 14];
+    var ribsprices = [15, 15, 15, 15, 15];
     var chops = ['Chicken Chop Suey', 'Beef Chop Suey', 'B.B.Q. Pork Chop Suey', 'Vegetables Chop Suey', 'Mushroom Chop Suey', 'Special Chop Suey', 'Bak Choy with Oyster Sauce', 'Prawn Chop Suey'];
-    var chopsprices = [13, 14, 13, 13, 13, 13, 13, 15];
+    var chopsprices = [14, 14, 14, 14, 14, 14, 14, 15];
     var fooyoungs = ['Chicken Egg Foo Young', 'B.B.Q. Pork Egg Foo Young', 'Mushrooms Egg Foo Young', 'Special Foo Young', 'Shrimp Egg Foo Young'];
     var fooyoungsprices = [12, 12, 12, 13, 13];
     var friedrices = ['B.B.Q. Pork Fried Rice', 'Chicken Fried Rice', 'Beef Fried Rice', 'Vegetables Fried Rice', 'Mushroom Fried Rice', 'Shrimps Fried Rice', 'Special Fried Rice', 'Chicken Mushroom Fried Rice', 'Curry Beef Fried Rice', 'Curry Chicken Fried Rice', 'Beef Tomato Fried Rice'];
     var friedricesprices = [11, 11, 11, 11, 11, 12, 12, 12, 12, 12, 12];
     var chickens = ['Almond Chicken', 'Lemon Chicken', 'Sweet & Sour Chicken', 'Chicken with Curry Sauce (Hot)', 'Chicken with Black Bean Sauce (Hot)', 'Chicken in Szechuan Sauce (Hot)', 'Almond Guy Ding', 'Cashew Guy Ding', 'Gung Bo Guy Ding (Hot)', 'Chicken with Bok Choy', 'Chicken with Broccoli'];
-    var chickensprices = [14, 14, 14, 13, 13, 13, 14, 14, 13, 13, 13];
+    var chickensprices = [15, 15, 15, 14, 14, 14, 14, 14, 14, 14, 14];
     var beefs = ['Beef with Broccoli', 'Beef with Bok Choy', 'Beef Green Beans Black Bean Sauce', 'Green Beans with Pepper Salt', 'Beef with Snow Peas', 'Beef with Curry Sauce (Hot)', 'Beef with Black Bean Sauce', 'Beef with Szechuan Sauce (Hot)', 'Beef with Black Pepper Sauce (Hot)', 'Ginger Fried Shredded Beef (Hot)', 'Beef with Fresh Tomato'];
     var beefsprices = [14, 14, 15, 15, 15, 14, 14, 14, 14, 15, 14];
     var seafoods = ['Deep Fried Prawns', 'Prawns with Broccoli', 'Prawns with Bok Choy', 'Prawns with Snow Peas', 'Prawns with Black Bean Sauce', 'Prawns in Szechuan Sauce', 'Prawns with Curry Sauce (Hot)', 'Prawns (Shell in) in Pepper Salt (Hot)', 'Deep Fried Squid in Pepper Sauce (Hot)', 'Squid with Black Bean Sauce (Hot)', 'Squid with Curry Sauce (Hot)', 'Scallops with Snow Peas', 'Scallops with Vegetable'];
     var seafoodsprices = [15, 15, 15, 16, 15, 15, 15, 16, 16, 15, 15, 16, 15];
     var chowmeins = ['Chicken Chow Mein', 'Beef Chow Mein', 'B.B.Q. Pork Chow Mein', 'Mushroom Chow Mein', 'Vegetables Chow Mein', 'Shrimps Chow Mein', 'Special Chow Mein', 'Chicken Mushroom Chow Mein', 'Curry Chicken Chow Mein', 'Curry Beef Chow Mein', 'Chicken Tomato Chow Mein', 'Beef Tomato Chow Mein', 'Shanghai Chow Mein', 'Singapore Fried Rice Noodle', 'Beef Chow Fun', 'Beef and Vegetables Chow Fun', ' Beef Chow Fun with Black Bean Sauce'];
-    var chowmeinsprices = [11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 13, 11, 13, 13, 13];
+    var chowmeinsprices = [11, 11, 11, 11, 11, 12, 12, 12, 12, 12, 13, 12, 13, 13, 13, 13, 14];
     var crispchowmeins = ['Cantonese Chow Mein (Crispy)', 'Ginger Beef Chow Mein (Crispy)', 'Chicken Chow Mein (Crispy)', 'Beef Chow Mein (Crispy)', 'Prawn Chow Mein (Crispy)'];
-    var crispchowmeinsprices = [14, 14, 14, 14, 15];
+    var crispchowmeinsprices = [15, 15, 15, 15, 16];
     var sides = ['Sweet and Sour Sauce', 'Curry Sauce', 'Lemon Sauce', 'Black Bean Sauce', 'Steamed Rice', 'French Fries', 'Seasame Seed', 'Hot Sauce', 'Hot Oil', 'Canned Pop'];
     var sidesprices = [3, 3, 3, 3, 3, 6, 1, 1, 1, 2.5];
 
